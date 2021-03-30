920421b5d4aa83483790ac055a82b1ab0171dc1a
Bartosz Dąbrowski <Bartoszeg02@gmail.com>
2021-03-31 00:05:32 +0200

    basic_steps file added

---
+++ tests_examples/demostore/features/steps/basic_steps.py
@@ -0,0 +1,16 @@
+from behave import given
+from selenium import webdriver
+
+
+@given('we are present on the {text} page')
+def go_to_page(context, text: str = None):
+    context.driver = webdriver.Chrome()
+    context.driver.implicitly_wait(5)
+    pages = {
+        "home": "http://demostore.supersqa.com",
+        "cart": "http://demostore.supersqa.com/cart/",
+        "checkout": "http://demostore.supersqa.com/checkout/",
+        "my account": "http://demostore.supersqa.com/my-account/",
+        "sample": "http://demostore.supersqa.com/sample-page/",
+    }
+    context.driver.get(pages[text]) if text in pages.keys() else context.driver.get("http://demostore.supersqa.com")
