dfa6344f464c4c33ce3c91838447751649050c75
Bartosz Dąbrowski <Bartoszeg02@gmail.com>
2021-04-02 13:51:41 +0200

    environment file added

--- tests_examples/demostore/features/steps/basic_steps.py
+++ tests_examples/demostore/features/steps/basic_steps.py
@@ -1,11 +1,8 @@
 from behave import given
-from selenium import webdriver
 
 
 @given("we are present on the {text} page")
 def go_to_page(context, text: str = None):
-    context.driver = webdriver.Chrome()
-    context.driver.implicitly_wait(5)
     pages = {
         "home": "http://demostore.supersqa.com",
         "cart": "http://demostore.supersqa.com/cart/",
